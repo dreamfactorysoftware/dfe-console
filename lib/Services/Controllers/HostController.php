0dccdf89b0e964d10382aa96a731bc095c92ee07
DreamFactory Operations <ops@dreamfactory.com>
2015-03-09 14:33:03 -0400

    Changed key names

--- lib/Services/Controllers/HostController.php
+++ lib/Services/Controllers/HostController.php
@@ -63,13 +63,13 @@ class HostController extends FactoryController
         }
 
         $_response = [
-            'cluster_id'          => $_instance->cluster_id,
-            'db_server_id'        => $_instance->db_server_id,
-            'app_server_id'       => $_instance->app_server_id,
-            'web_server_id'       => $_instance->web_server_id,
-            'owner_id'            => $_instance->user_id,
-            'owner_email_address' => $_instance->user->email_addr_text,
-            'instance_id'         => $_instance->user->id,
+            'cluster-id'          => $_instance->cluster_id,
+            'db-server-id'        => $_instance->db_server_id,
+            'app-server-id'       => $_instance->app_server_id,
+            'web-server-id'       => $_instance->web_server_id,
+            'owner-id'            => $_instance->user_id,
+            'owner-email-address' => $_instance->user->email_addr_text,
+            'instance-id'         => $_instance->user->id,
         ];
 
         return $_response;
