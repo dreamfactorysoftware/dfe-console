ccb8039ed5d099268f701782cbda141bd5d24c66
Jerry Ablan <jerryablan@dreamfactory.com>
2015-03-12 17:15:28 -0400

    Current work checkpoint. See diff for details.

--- config/provisioners.php
+++ config/provisioners.php
@@ -7,5 +7,5 @@ return [
     'rave' => [
         'instance' => 'DreamFactory\\Enterprise\\Services\\Provisioners\\RaveProvisioner',
         'storage'  => 'DreamFactory\\Enterprise\\Services\\Provisioners\\RaveStorageProvisioner',
-    ],df
+    ],
 ];
