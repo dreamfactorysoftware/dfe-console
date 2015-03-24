c1f0bc7d6bcce334633967f4de5bc597255624d1
Jerry Ablan <jerryablan@dreamfactory.com>
2015-03-24 19:15:00 -0400

    Composer dependencies updated.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,19 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    @section('head-master')
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@yield('page-title', 'Welcome!') | DreamFactory Enterprise&trade;</title>
-        <link href="/vendor/dfe-common/static/bootswatch-3.3.4/{{ config('dfe.common.themes.page', 'darkly') }}.min.css" rel="stylesheet">
-        <link href="/static/font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet">
-        <link href="/css/style.css" rel="stylesheet">
-        <!--[if lt IE 9]>
-        <script src="//oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
-        <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-        <![endif]-->
-    @show
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('page-title', 'Welcome!') | DreamFactory Enterprise&trade;</title>
+    <link href="/vendor/dfe-common/static/bootswatch-3.3.4/{{ config('dfe.common.themes.page', 'flatly') }}.min.css" rel="stylesheet">
+    <link href="/static/font-awesome-4.3.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="//oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js"></script>
+    <script src="//oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script><![endif]-->
 
     @section('head-links')
     @show
