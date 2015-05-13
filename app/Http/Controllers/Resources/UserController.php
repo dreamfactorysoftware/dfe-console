614161541b5f76a073c4b58f70bdca494ef59bd8
Carsten Jacobsen <carstenjacobsen_dreamfactory@Carstens-MacBook-Pro.local>
2015-05-13 12:05:43 -0700

    User Controller create() updated

--- app/Http/Controllers/Resources/UserController.php
+++ app/Http/Controllers/Resources/UserController.php
@@ -40,7 +40,7 @@ class UserController extends ResourceController //FactoryController //
     /**
      * @return $this
      */
-    public function create( $viewData = [] )
+    public function create( array $viewData = [] )
     {
         return \View::make( 'app.users.create' )->with( 'prefix', $this->_prefix );
     }
