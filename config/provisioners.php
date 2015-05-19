1624063755c228ca2e2015c32bb5bce5ec7c4408
Jerry Ablan <jerryablan@dreamfactory.com>
2015-05-19 10:55:46 -0400

    Current work checkpoint. See diff for details.

--- config/provisioners.php
+++ config/provisioners.php
@@ -18,10 +18,9 @@ return [
                 'instance-version' => [
                     'name'       => 'Version',
                     'help-block' => 'If you wish, you may choose a different version of the DSP to provision.',
-                    'suggested'  => '1.9.x-dev',
+                    'suggested'  => '1.10.x-dev',
                     'items'      => [
-//                        '1.9.2'     => ['document-root' => '/var/www/_releases/dsp-core/1.9.2/web', 'description' => 'DSP v1.9.2',],
-                        '1.9.x-dev' => ['document-root' => '/var/www/_releases/dsp-core/1.9.x-dev/web', 'description' => 'DSP v1.9.x-dev',],
+                        '1.10.x-dev' => ['document-root' => '/var/www/_releases/dsp-core/1.9.x-dev/web', 'description' => 'DSP v1.10.x-dev',],
                     ],
                 ],
             ],
