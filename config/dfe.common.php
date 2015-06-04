9ed777a2faba26fe47a1b1ac38d9067a60466d99
Jerry Ablan <jerryablan@dreamfactory.com>
2015-06-04 18:08:19 -0400

    Fixed config files

--- config/dfe.common.php
+++ config/dfe.common.php
@@ -7,22 +7,13 @@ return [
     //******************************************************************************
     //* Global Options
     //******************************************************************************
-    'display-name'         => 'DreamFactory Enterprise&trade; Console',
-    'display-version'      => 'v1.0.x-alpha',
-    'display-copyright'    => '© DreamFactory Software, Inc. 2012-' . date( 'Y' ) . '. All Rights Reserved.',
+    'display-name'      => 'DreamFactory Enterprise&trade; Console',
+    'display-version'   => 'v1.0.x-alpha',
+    'display-copyright' => '© DreamFactory Software, Inc. 2012-' . date( 'Y' ) . '. All Rights Reserved.',
     /**
      * Theme selection -- a bootswatch theme name
      * Included are cerulean, darkly, flatly, paper, and superhero.
      * You may also install other compatible themes and use them as well.
      */
-    'themes'               => ['auth' => 'darkly', 'page' => 'flatly'],
-    //******************************************************************************
-    //* Custom Logging
-    //******************************************************************************
-    /** If these two are specified, log-path and log-file-name will be used instead of the default "storage/logs/laravel.log" */
-    'old-log-config-class' => 'Illuminate\\Foundation\\Bootstrap\\ConfigureLogging',
-    'new-log-config-class' => 'DreamFactory\\Enterprise\\Common\\Bootstrap\\ConfigureCommonLogging',
-    /** The log path and file name to use if the above two are specified */
-    'log-path'             => env( 'DFE_LOG_PATH' ),
-    'log-file-name'        => env( 'DFE_LOG_FILE_NAME' ),
+    'themes'            => ['auth' => 'darkly', 'page' => 'flatly'],
 ];
