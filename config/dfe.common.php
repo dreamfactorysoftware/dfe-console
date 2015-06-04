0c6547f136eb338c8b8b18cf91aae54ff549a674
Jerry Ablan <jerryablan@dreamfactory.com>
2015-06-04 17:31:15 -0400

    Composer dependencies updated.

--- config/dfe.common.php
+++ config/dfe.common.php
@@ -21,8 +21,8 @@ return [
     //******************************************************************************
     /** If these two are specified, log-path and log-file-name will be used instead of the default "storage/logs/laravel.log" */
     'old-log-config-class' => 'Illuminate\\Foundation\\Bootstrap\\ConfigureLogging',
-    'new-log-config-class' => 'DreamFactory\\Enterprise\\Common\\Bootstrap\\CommonLoggingConfiguration',
+    'new-log-config-class' => 'DreamFactory\\Enterprise\\Common\\Bootstrap\\ConfigureCommonLogging',
     /** The log path and file name to use if the above two are specified */
-    'log-path'             => env( 'DFE_LOG_PATH', '/data/logs/console' ),
-    'log-file-name'        => env( 'DFE_LOG_FILE_NAME', 'laravel.log' ),
+    'log-path'             => env( 'DFE_LOG_PATH' ),
+    'log-file-name'        => env( 'DFE_LOG_FILE_NAME' ),
 ];
