43c5ca4661c9efaff15444a7d0f0b014c71be52d
Jerry Ablan <jerryablan@dreamfactory.com>
2015-06-05 15:06:03 -0400

    Changed the format of the links.php config file

--- config/links.php
+++ config/links.php
@@ -5,8 +5,8 @@
 //******************************************************************************
 
 return [
-    'Welcome'    => ['href' => '//www.dreamfactory.com/in_product_welcome.html', 'alt' => null, 'attributes' => [],],
-    'Quickstart' => ['href' => '#', 'attributes' => [],],
-    'Resources'  => ['href' => '//www.dreamfactory.com/in_product_resources.html', 'attributes' => [],],
-    'Download'   => ['href' => '//www.dreamfactory.com/in_product_downloads.html', 'attributes' => [],],
+    ['name' => 'Welcome', 'href' => '//www.dreamfactory.com/in_product_welcome.html', 'attributes' => [],],
+    ['name' => 'Quickstart', 'href' => '#', 'attributes' => [],],
+    ['name' => 'Resources', 'href' => '//www.dreamfactory.com/in_product_resources.html', 'attributes' => [],],
+    ['name' => 'Download', 'href' => '//www.dreamfactory.com/in_product_downloads.html', 'attributes' => [],],
 ];
