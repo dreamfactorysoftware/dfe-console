0c51a9ece7fe65fc793d55e5b205e76aa13b9dda
Jerry Ablan <jerryablan@dreamfactory.com>
2015-06-08 10:28:01 -0400

    Current work checkpoint. See diff for details.

--- database/migrations/2015_04_13_195050_create_snapshot_table.php
+++ database/migrations/2015_04_13_195050_create_snapshot_table.php
@@ -23,13 +23,24 @@ class CreateSnapshotTable extends Migration
                 function ( Blueprint $table )
                 {
                     $table->bigIncrements( 'id' );
-                    $table->string( 'snapshot_id_text', 128 )->index();
                     $table->integer( 'user_id' )->index();
                     $table->integer( 'instance_id' )->index();
-                    $table->string( 'url_text', 1024 );
+                    $table->integer( 'route_hash_id' )->index();
+                    $table->string( 'snapshot_id_text', 128 )->index()->nullable();
+                    $table->boolean( 'public_ind' )->default( true );
+                    $table->string( 'public_url_text', 1024 );
                     $table->dateTime( 'expire_date' );
+
                     $table->dateTime( 'create_date' );
                     $table->timestamp( 'lmod_date' )->default( \DB::raw( 'CURRENT_TIMESTAMP' ) );
+
+                    //  Make snapshot ID unique for user
+                    $table->unique( ['user_id', 'snapshot_id_text'] );
+
+                    //  Foreign keys
+                    $table->foreign( 'user_id' )->references( 'id' )->on( 'user_t' )->onDelete( 'cascade' );
+                    $table->foreign( 'instance_id' )->references( 'id' )->on( 'instance_t' )->onDelete( 'cascade' );
+                    $table->foreign( 'route_hash_id' )->references( 'id' )->on( 'route_hash_t' )->onDelete( 'cascade' );
                 }
             );
         }
