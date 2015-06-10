47b10c66b952d816575aea6ec1a7e3ec8b942ca1
Jerry Ablan <jerryablan@dreamfactory.com>
2015-06-10 13:37:42 -0400

    Schema updated

--- database/migrations/2015_04_13_195050_create_snapshot_table.php
+++ database/migrations/2015_04_13_195050_create_snapshot_table.php
@@ -16,36 +16,29 @@ class CreateSnapshotTable extends Migration
      */
     public function up()
     {
-        if ( !\Schema::hasTable( 'snapshot_t' ) )
-        {
+        if (!\Schema::hasTable('snapshot_t')) {
             \Schema::create(
                 'snapshot_t',
-                function ( Blueprint $table )
-                {
-                    $table->bigIncrements( 'id' );
-                    $table->integer( 'user_id' );
-                    $table->integer( 'instance_id' );
-                    $table->integer( 'route_hash_id' );
-                    $table->string( 'snapshot_id_text', 128 )->index();
-                    $table->boolean( 'public_ind' )->default( true );
-                    $table->string( 'public_url_text', 1024 );
-                    $table->dateTime( 'expire_date' );
+                function (Blueprint $table){
+                    $table->bigIncrements('id');
+                    $table->integer('user_id')->index();
+                    $table->integer('instance_id')->index();
+                    $table->integer('route_hash_id')->index();
+                    $table->string('snapshot_id_text', 128)->index();
+                    $table->boolean('public_ind')->default(true);
+                    $table->string('public_url_text', 1024);
+                    $table->dateTime('expire_date');
 
-                    $table->dateTime( 'create_date' );
-                    $table->timestamp( 'lmod_date' )->default( \DB::raw( 'CURRENT_TIMESTAMP' ) );
+                    $table->dateTime('create_date');
+                    $table->timestamp('lmod_date')->default(\DB::raw('CURRENT_TIMESTAMP'));
 
                     //  Make snapshot ID unique for user
-                    $table->unique( ['user_id', 'snapshot_id_text'] );
+                    $table->unique(['user_id', 'snapshot_id_text']);
 
                     //  Foreign keys
-                    $table->index( 'user_id' );
-                    $table->foreign( 'user_id' )->references( 'id' )->on( 'user_t' )->onDelete( 'cascade' );
-
-                    $table->index( 'instance_id' );
-                    $table->foreign( 'instance_id' )->references( 'id' )->on( 'instance_t' )->onDelete( 'cascade' );
-
-                    $table->index( 'route_hash_id' );
-//                    $table->foreign( 'route_hash_id' )->references( 'id' )->on( 'route_hash_t' )->onDelete( 'cascade' );
+                    $table->foreign('user_id')->references('id')->on('user_t')->onDelete('cascade');
+                    $table->foreign('instance_id')->references('id')->on('instance_t')->onDelete('cascade');
+                    $table->foreign('route_hash_id')->references('id')->on('route_hash_t')->onDelete('cascade');
                 }
             );
         }
@@ -58,9 +51,8 @@ class CreateSnapshotTable extends Migration
      */
     public function down()
     {
-        if ( \Schema::hasTable( 'snapshot_t' ) )
-        {
-            \Schema::drop( 'snapshot_t' );
+        if (\Schema::hasTable('snapshot_t')) {
+            \Schema::drop('snapshot_t');
         }
     }
 
