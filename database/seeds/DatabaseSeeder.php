f4422b7c2c070d76d6656d1cb78e7ca33f7b092b
Jerry Ablan <jerryablan@dreamfactory.com>
2015-06-10 17:14:49 -0400

    Added lookup table seed data to schema

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,20 +1,22 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder {
+class DatabaseSeeder extends Seeder
+{
+    //******************************************************************************
+    //* Methods
+    //******************************************************************************
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Model::unguard();
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        Model::unguard();
 
-		// $this->call('UserTableSeeder');
-	}
+        //$this->call('LookupSeeder');
+    }
 
 }
