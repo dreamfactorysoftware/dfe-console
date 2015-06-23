75d7715327dee9cf317b8f39c007ee5ccd4b0cb0
Carsten Jacobsen <carstenjacobsen_dreamfactory@Carstens-MacBook-Pro.local>
2015-06-22 23:32:08 -0700

    Added Servers error messages and validation

--- resources/views/app/servers/create.blade.php
+++ resources/views/app/servers/create.blade.php
@@ -74,96 +74,99 @@
 
                                             <div class="row">
                                                 <div class="col-md-6">
+                                                    @if(Session::has('flash_message'))
+                                                        <p class="alert {{ Session::get('flash_type') }}">{{ Session::get('flash_message') }}</p>
+                                                    @endif
                                                     <div class="form-group">
                                                         <label>Name</label>
-                                                        <input id="server_id_text" name="server_id_text" class="form-control" placeholder="Enter server name." type="name" required>
+                                                        <input id="server_id_text" name="server_id_text" class="form-control" placeholder="Enter server name." type="name" value="{{ Input::old('server_id_text') }}" required>
                                                     </div>
                                                     <div class="form-group">
                                                         <label>Type</label>
                                                         <select class="form-control" id="server_type_select" name="server_type_select" required>
                                                             <option value="">Select type</option>
                                                             @foreach ($server_types as $server_type)
-                                                                <option id="{{$server_type['id']}}">{{$server_type['type_name_text']}}</option>
+                                                                <option id="{{$server_type['id']}}" @if (Input::old('server_type_select') == $server_type['type_name_text']) selected="selected" @endif>{{$server_type['type_name_text']}}</option>
                                                             @endforeach
                                                         </select>
                                                     </div>
                                                     <div class="form-group">
                                                         <label>Host</label>
-                                                        <input id="host_text" name="host_text" class="form-control" placeholder="Enter host." type="text" required>
+                                                        <input id="host_text" name="host_text" class="form-control" placeholder="Enter host." type="text" value="{{ Input::old('host_text') }}" required>
                                                     </div>
                                                     <div id="server_type_db" style="display: none;">
                                                         <div class="form-group">
                                                             <label>Port</label>
-                                                            <input id="db_port_text" name="config[db][port]" class="form-control" placeholder="Enter port." type="text">
+                                                            <input id="db_port_text" name="config[db][port]" class="form-control" placeholder="Enter port." value="{{ Input::old('config.db.port') }}" type="number">
                                                         </div>
                                                         <div class="form-group">
                                                             <label>User Name</label>
-                                                            <input id="db_username_text" name="config[db][username]" class="form-control" placeholder="Enter user name." type="text">
+                                                            <input id="db_username_text" name="config[db][username]" class="form-control" placeholder="Enter user name." value="{{ Input::old('config.db.username') }}" type="text">
                                                         </div>
                                                         <div class="form-group">
                                                             <label>Password</label>
-                                                            <input id="db_password_text" name="config[db][password]" class="form-control" placeholder="Enter password." type="password">
+                                                            <input id="db_password_text" name="config[db][password]" class="form-control" placeholder="Enter password." value="{{ Input::old('config.db.password') }}" type="password">
                                                         </div>
                                                         <div class="form-group">
                                                             <label>Driver</label>
-                                                            <input id="db_driver_text" name="config[db][driver]" class="form-control" placeholder="Enter database driver." type="text">
+                                                            <input id="db_driver_text" name="config[db][driver]" class="form-control" placeholder="Enter database driver." value="{{ Input::old('config.db.driver') }}" type="text">
                                                         </div>
                                                         <div class="form-group">
                                                             <label>Default Database Name</label>
-                                                            <input id="db_default_db_name_text" name="config[db][default-database-name]" class="form-control" placeholder="Enter default database name." type="text">
+                                                            <input id="db_default_db_name_text" name="config[db][default-database-name]" class="form-control" placeholder="Enter default database name." value="{{ Input::old('config.db.default-database-name') }}" type="text">
                                                         </div>
                                                         <div class="form-group">
                                                             <label>Server Assignment</label><br>
-                                                                <input id="db_multi_asgn_text" type="checkbox" id="db_multi_asgn_text" name="config[db][multi-assign]">
+                                                                <input id="db_multi_asgn_text" type="checkbox" id="db_multi_asgn_text" name="config[db][multi-assign]" @if (Input::old('config.db.multi-assign') == 'on') checked @endif>
                                                                     Allow Multiple Server Assignments
                                                         </div>
                                                     </div>
                                                     <div id="server_type_web" style="display: none;">
                                                         <div class="form-group">
                                                             <label>Port</label>
-                                                            <input id="web_port_text" name="config[web][port]" class="form-control" placeholder="Enter port." type="text">
+                                                            <input id="web_port_text" name="config[web][port]" class="form-control" placeholder="Enter port." value="{{ Input::old('config.web.port') }}" type="text">
                                                         </div>
                                                         <div class="form-group">
                                                             <label>Scheme</label>
                                                             <select id="web_scheme_text" name="config[web][scheme]" class="form-control">
                                                                 <option value="" disabled selected>Select scheme</option>
-                                                                <option value="http">HTTP</option>
-                                                                <option value="https">HTTPS</option>
+                                                                <option value="http" @if (Input::old('config.web.scheme') == 'http') selected="selected" @endif>HTTP</option>
+                                                                <option value="https" @if (Input::old('config.web.scheme') == 'https') selected="selected" @endif>HTTPS</option>
                                                             </select>
                                                         </div>
                                                         <div class="form-group">
                                                             <label>User Name</label>
-                                                            <input id="web_username_text" name="config[web][username]" class="form-control" placeholder="Enter user name." type="text">
+                                                            <input id="web_username_text" name="config[web][username]" class="form-control" placeholder="Enter user name." value="{{ Input::old('config.web.username') }}" type="text">
                                                         </div>
                                                         <div class="form-group">
                                                             <label>Password</label>
-                                                            <input id="web_password_text" name="config[web][password]" class="form-control" placeholder="Enter password." type="password">
+                                                            <input id="web_password_text" name="config[web][password]" class="form-control" placeholder="Enter password." value="{{ Input::old('config.web.password') }}" type="password">
                                                         </div>
                                                     </div>
                                                     <div id="server_type_app" style="display: none;">
                                                         <div class="form-group">
                                                             <label>Port</label>
-                                                            <input id="app_port_text" name="config[app][port]" class="form-control" placeholder="Enter port." type="text">
+                                                            <input id="app_port_text" name="config[app][port]" class="form-control" placeholder="Enter port." value="{{ Input::old('config.app.port') }}" type="text">
                                                         </div>
                                                         <div class="form-group">
                                                             <label>Scheme</label>
                                                             <select id="app_scheme_text" name="config[app][scheme]" class="form-control">
                                                                 <option value="" disabled selected>Select scheme</option>
-                                                                <option value="http">HTTP</option>
-                                                                <option value="https">HTTPS</option>
+                                                                <option value="http" @if (Input::old('config.app.scheme') == 'http') selected="selected" @endif>HTTP</option>
+                                                                <option value="https" @if (Input::old('config.app.scheme') == 'https') selected="selected" @endif>HTTPS</option>
                                                             </select>
                                                         </div>
                                                         <div class="form-group">
                                                             <label>User Name</label>
-                                                            <input id="app_username_text" name="config[app][username]" class="form-control" placeholder="Enter user name." type="text">
+                                                            <input id="app_username_text" name="config[app][username]" class="form-control" placeholder="Enter user name." value="{{ Input::old('config.app.username') }}" type="text">
                                                         </div>
                                                         <div class="form-group">
                                                             <label>Password</label>
-                                                            <input id="app_password_text" name="config[app][password]" class="form-control" placeholder="Enter password." type="password">
+                                                            <input id="app_password_text" name="config[app][password]" class="form-control" placeholder="Enter password." value="{{ Input::old('config.app.password') }}" type="password">
                                                         </div>
                                                         <div class="form-group">
                                                             <label>Access Token</label>
-                                                            <input id="app_accesstoken_text" name="config[app][access_token]" class="form-control" placeholder="Enter access token." type="password">
+                                                            <input id="app_accesstoken_text" name="config[app][access_token]" class="form-control" placeholder="Enter access token." value="{{ Input::old('config.app.access_token') }}" type="password">
                                                         </div>
                                                     </div>
                                                 </div>
