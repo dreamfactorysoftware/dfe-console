cb08a790d319f9b87672af4c5a6ef4db827734c4
David Weiner <davidweiner@dreamfactory.com>
2015-08-02 00:39:56 -0400

    starting limit retreival for the index blade

--- resources/views/app/limits.blade.php
+++ resources/views/app/limits.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     @include('layouts.partials.sidebar-menu',['resource'=>'limits'])
 <!--
-{{ print_r($limits, true) }}
+{{{ print_r($limits, true) }}}
 -->
     <div class="col-xs-11 col-sm-10 col-md-10">
         @include('layouts.partials.context-header',['resource'=>'policies','title' => 'Policy Manager'])
