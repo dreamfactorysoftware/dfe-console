81f565e0141a31116a3b50e431314df118b12007
Carsten Jacobsen <carstenjacobsen_dreamfactory@Carstens-MacBook-Pro.local>
2015-08-05 22:26:42 -0700

    [DFE-136] Updated Limits UI

--- app/Http/Controllers/Resources/LimitController.php
+++ app/Http/Controllers/Resources/LimitController.php
@@ -437,7 +437,7 @@ class LimitController extends ResourceController
             'cluster_id_text' => $_values['cluster_id_text'],
             'instance_id' => $_limit['instance_id'],
             'instance_id_text' => $_values['instance_id_text'],
-            'user_id' => $_limit['user_id'],
+            'user_id' => $_values['user_id'],
             //'user_id_text' => $_values['user_id_text'],
             //'service_desc' => empty($_values['service_name']) === true ?'':$_services[$_values['service_name']],
             'user_name' => $_values['user_id'] == 0 ?'':$_users[$_values['user_id']],
