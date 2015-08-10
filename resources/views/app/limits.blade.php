b5506c1a43ae9dac9d73922bb2bd0b6756cd9cc6
Carsten Jacobsen <carstenjacobsen_dreamfactory@Carstens-MacBook-Pro.local>
2015-08-10 14:06:45 -0700

    UI Cleanup

--- resources/views/app/limits.blade.php
+++ resources/views/app/limits.blade.php
@@ -4,7 +4,7 @@
     @include('layouts.partials.sidebar-menu',['resource'=>'limits'])
 
     <div class="col-xs-11 col-sm-10 col-md-10">
-        @include('layouts.partials.context-header',['resource'=>'policies','title' => 'Limit Manager'])
+        @include('layouts.partials.context-header',['resource'=>'policies','title' => 'Manage Limits'])
 
     <!-- Tool Bar -->
     <div class="row">
