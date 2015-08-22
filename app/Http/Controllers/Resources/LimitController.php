0146418eaa68611ca679dc92c4f28fed822f282a
Carsten Jacobsen <carstenjacobsen_dreamfactory@Carstens-MacBook-Pro.local>
2015-08-21 21:30:15 -0700

    Updated Limit controller

--- app/Http/Controllers/Resources/LimitController.php
+++ app/Http/Controllers/Resources/LimitController.php
@@ -170,7 +170,7 @@ class LimitController extends ResourceController
                     ] as $_input_key => $_input_default) {
                 $_input[$_input_key] = \Input::get($_input_key, $_input_default);
             }
-echo json_encode($_input);
+
             $_time_period = str_replace(' ', '-', strtolower($_input['period_name']));
 
             if ( $_input['cluster_id'] === '' && $_input['instance_id'] === '') {
@@ -197,15 +197,15 @@ echo json_encode($_input);
                 'label_text' => $_input['label_text']
             ];
 
-            /*
+
             if (!Limit::find($id)->update($limit)) {
                 throw new EnterpriseDatabaseException('Unable to update limit "' . $id . '"');
             }
-*/
+
             \Session::flash('flash_message', 'The limit "' . $_input['label_text'] . '" was updated successfully!');
             \Session::flash('flash_type', 'alert-success');
 
-  //          return \Redirect::to($this->makeRedirectUrl('limits'));
+            return \Redirect::to($this->makeRedirectUrl('limits'));
 
         } catch (QueryException $e) {
 
