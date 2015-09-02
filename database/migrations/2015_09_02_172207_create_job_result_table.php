849c787ed4f4beb1ca1e7a359a22738058d93f7a
Jerry Ablan <jerryablan@dreamfactory.com>
2015-09-02 17:26:02 -0400

    New job_result_t migration

---
+++ database/migrations/2015_09_02_172207_create_job_result_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateJobResultTable extends Migration
+{
+    //******************************************************************************
+    //* Methods
+    //******************************************************************************
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('job_result_t')) {
+            Schema::create('job_result_t',
+                function (Blueprint $table){
+                    $table->bigIncrements('id');
+                    /** @noinspection PhpUndefinedMethodInspection */
+                    $table->string('result_id_text', 256)->index();
+                    $table->mediumText('result_text');
+                    $table->dateTime('create_date');
+                    $table->timestamp('lmod_date')->default(\DB::raw('CURRENT_TIMESTAMP'));
+                });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable('job_result_t')) {
+            Schema::drop('job_result_t');
+        }
+    }
+}
