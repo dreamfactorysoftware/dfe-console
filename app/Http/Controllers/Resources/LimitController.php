b40b1844777571d40f6281062a13a6c963712a6d
Carsten Jacobsen <carstenjacobsen_dreamfactory@Carstens-MacBook-Pro.local>
2015-09-03 15:51:29 -0700

    Updated limit

--- app/Http/Controllers/Resources/LimitController.php
+++ app/Http/Controllers/Resources/LimitController.php
@@ -306,10 +306,13 @@ class LimitController extends ResourceController
                     $_instance = $this->_findInstance($_limit['instance_id']);
                     $_values['instance_id_text'] = $_instance->instance_id_text;
 
-                    logger('LimitController (instance): '.$_instance.' / '.$_values['instance_id_text']);
+                    //logger('LimitController (instance): '.$_instance.' / '.$_values['instance_id_text']);
 
                     if ('user' == $_this_limit_type) {
                         if (false !== ($_rows = $this->getInstanceUsers($_instance))) {
+
+                            logger('LimitController (getuser): '.$_rows);
+
                             foreach ($_rows as $_user) {
                                 logger('LimitController: '.$_user['id'].' / '.$_values['user_id'].' / '.$_user['name']);
                                 if ($_user['id'] != $_values['user_id']) {
