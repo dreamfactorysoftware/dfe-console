ebc36d5ecd66db4f9f37d5e6bf2cb088ec735614
Jerry Ablan <jerryablan@dreamfactory.com>
2015-09-07 20:36:48 -0400

    Update command work

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         //  Core
+        'DreamFactory\Enterprise\Console\Console\Commands\Update',
         'DreamFactory\Enterprise\Console\Console\Commands\Setup',
         'DreamFactory\Enterprise\Console\Console\Commands\Register',
         'DreamFactory\Enterprise\Console\Console\Commands\Mount',
