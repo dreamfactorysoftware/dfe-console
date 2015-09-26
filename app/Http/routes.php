2c118963b780c35248c1effd9d0bc738d5c55604
Jerry Ablan <jerryablan@dreamfactory.com>
2015-09-25 21:12:09 -0400

    Current work checkpoint. See diff for details.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,18 +12,15 @@ if (true === config('dfe.enable-console-api', false)) {
         function (){
             \Route::controller('ops', 'OpsController');
 
-            \Route::resource('user', DreamFactory\Enterprise\Console\Http\Controllers\Ops\UserController::class);
-            \Route::resource('service-user',
-                DreamFactory\Enterprise\Console\Http\Controllers\Ops\ServiceUserController::class);
-            \Route::resource('server', DreamFactory\Enterprise\Console\Http\Controllers\Ops\ServerController::class);
-            \Route::resource('cluster', DreamFactory\Enterprise\Console\Http\Controllers\Ops\ClusterController::class);
-            \Route::resource('instance',
-                DreamFactory\Enterprise\Console\Http\Controllers\Ops\InstanceController::class);
-            \Route::resource('mount', DreamFactory\Enterprise\Console\Http\Controllers\Ops\MountController::class);
-            \Route::resource('app-key', DreamFactory\Enterprise\Console\Http\Controllers\Ops\AppKeyController::class);
-            \Route::resource('instance',
-                DreamFactory\Enterprise\Console\Http\Controllers\Ops\InstanceController::class);
-            \Route::resource('limit', DreamFactory\Enterprise\Console\Http\Controllers\Ops\LimitController::class);
+            \Route::resource('users', 'Ops\UserController');
+            \Route::resource('service-users', 'Ops\ServiceUserController');
+            \Route::resource('servers', 'Ops\ServerController');
+            \Route::resource('clusters', 'Ops\ClusterController');
+            \Route::resource('instances', 'Ops\InstanceController');
+            \Route::resource('mounts', 'Ops\MountController');
+            \Route::resource('app-keys', 'Ops\AppKeyController');
+            \Route::resource('instances', 'Ops\InstanceController');
+            \Route::resource('limits', 'Ops\LimitController');
         });
 }
 
@@ -36,15 +33,15 @@ use DreamFactory\Enterprise\Console\Enums\ConsoleDefaults;
 
 \Route::group(['middleware' => 'auth'],
     function (){
-        \Route::get(ConsoleDefaults::UI_PREFIX, ['as' => 'home', 'uses' => 'Resources\\HomeController@index']);
-        \Route::get('/home', ['as' => 'home', 'uses' => 'Resources\\HomeController@index']);
-        \Route::get('/', ['as' => 'home', 'uses' => 'Resources\\HomeController@index']);
+        \Route::get(ConsoleDefaults::UI_PREFIX, ['as' => 'home', 'uses' => 'Resources\HomeController@index']);
+        \Route::get('/home', ['as' => 'home', 'uses' => 'Resources\HomeController@index']);
+        \Route::get('/', ['as' => 'home', 'uses' => 'Resources\HomeController@index']);
         \Route::get(ConsoleDefaults::UI_PREFIX . '/cluster/{clusterId}/instances',
-            'Resources\\ClusterController@getInstances');
+            'Resources\ClusterController@getInstances');
         \Route::get(ConsoleDefaults::UI_PREFIX . '/instance/{instanceId}/services',
-            'Resources\\LimitController@getInstanceServices');
+            'Resources\LimitController@getInstanceServices');
         \Route::get(ConsoleDefaults::UI_PREFIX . '/instance/{instanceId}/users',
-            'Resources\\LimitController@getInstanceUsers');
+            'Resources\LimitController@getInstanceUsers');
     });
 
 //******************************************************************************
@@ -53,19 +50,17 @@ use DreamFactory\Enterprise\Console\Enums\ConsoleDefaults;
 
 \Route::group([
     'prefix'     => ConsoleDefaults::UI_PREFIX,
+    'namespace'  => 'Resources',
     'middleware' => 'auth',
 ],
     function (){
-        \Route::resource('home', DreamFactory\Enterprise\Console\Http\Controllers\Resources\HomeController::class);
-        \Route::resource('users', DreamFactory\Enterprise\Console\Http\Controllers\Resources\UserController::class);
-        \Route::resource('servers', DreamFactory\Enterprise\Console\Http\Controllers\Resources\ServerController::class);
-        \Route::resource('clusters',
-            DreamFactory\Enterprise\Console\Http\Controllers\Resources\ClusterController::class);
-        \Route::resource('instances',
-            DreamFactory\Enterprise\Console\Http\Controllers\Resources\InstanceController::class);
-        \Route::resource('limits',
-            DreamFactory\Enterprise\Console\Http\Controllers\Resources\LimitController::class);
-        \Route::resource('reports', DreamFactory\Enterprise\Console\Http\Controllers\Resources\ReportController::class);
+        \Route::resource('home', 'HomeController');
+        \Route::resource('users', 'UserController');
+        \Route::resource('servers', 'ServerController');
+        \Route::resource('clusters', 'ClusterController');
+        \Route::resource('instances', 'InstanceController');
+        \Route::resource('limits', 'LimitController');
+        \Route::resource('reports', 'ReportController');
     });
 
 //******************************************************************************
@@ -76,8 +71,8 @@ use DreamFactory\Enterprise\Console\Enums\ConsoleDefaults;
 \Route::controllers([
     'dashboard' => 'DashboardController',
     'settings'  => 'SettingsController',
-    'auth'      => 'Auth\\AuthController',
-    'password'  => 'Auth\\PasswordController',
+    'auth'      => 'Auth\AuthController',
+    'password'  => 'Auth\PasswordController',
 ]);
 
 /** An endpoint to return the current version of dfe-console */
