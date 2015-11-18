1f0e01971b590fd3ed6ada891b891460030f41fd
Jerry Ablan <jerryablan@dreamfactory.com>
2015-11-18 12:44:42 -0500

    Updated link check

--- app/Http/Controllers/Resources/HomeController.php
+++ app/Http/Controllers/Resources/HomeController.php
@@ -84,7 +84,6 @@ class HomeController extends FactoryController
     {
         $_links = [];
         $_rawLinks = config('links.console', []);
-        \Log::debug('Raw links: ' . print_r($_rawLinks, true));
 
         //  Override links to add link parameters if requested
         foreach ($_rawLinks as $_index => $_link) {
@@ -100,6 +99,8 @@ class HomeController extends FactoryController
             //  Only show links that are supposed to be shown...
             $_links[] = $_link;
         }
+
+        \Log::debug('links: ' . print_r($_links, true));
 
         return $_links;
     }
