520c77a432d68057db49aa7ee54b4e672415f481
Carsten Jacobsen <carstenjacobsen_dreamfactory@Carstens-MacBook-Pro.local>
2015-11-25 21:04:20 -0800

    Limit work

--- app/Http/Controllers/Resources/LimitController.php
+++ app/Http/Controllers/Resources/LimitController.php
@@ -714,7 +714,8 @@ class LimitController extends ViewController
         if (!empty($instanceId)) {
             $_instance = ($instanceId instanceof Instance) ? $instanceId : $this->_findInstance($instanceId);
 
-            return $this->formatResponse($_instance->call('/api/v2/system/user', [], [], Request::METHOD_GET, false));
+            return $this->formatResponse($_instance->call(Request::METHOD_GET, '/api/v2/system/user'));
+            //return $this->formatResponse($_instance->call('/api/v2/system/user', [], [], Request::METHOD_GET, false));
         }
 
         return false;
