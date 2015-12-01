c2ea11f52e10d73f6948ed47a848268f1c8030ff
Jerry Ablan <jerryablan@dreamfactory.com>
2015-12-01 13:16:43 -0500

    Changed base class to match exportjob

--- lib/Services/Jobs/ImportJob.php
+++ lib/Services/Jobs/ImportJob.php
@@ -1,6 +1,6 @@
 <?php namespace DreamFactory\Enterprise\Services\Jobs;
 
-class ImportJob extends PortabilityJob
+class ImportJob extends PortableJob
 {
     //******************************************************************************
     //* Constants
