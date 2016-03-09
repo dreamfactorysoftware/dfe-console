6722b79ff8901f1ecaec5a3604c7bf2e3eedd612
Jerry Ablan <jerryablan@dreamfactory.com>
2016-03-08 19:31:12 -0500

    release/1.0.20 prep

--- config/dfe.php
+++ config/dfe.php
@@ -71,7 +71,7 @@ return [
     //******************************************************************************
     'common'                  => [
         'display-name'       => 'DreamFactory Enterprise&trade; Console',
-        'display-version'    => env('DFE_VERSION', '1.0.19'),
+        'display-version'    => env('DFE_VERSION', '1.0.20'),
         'display-copyright'  => '© DreamFactory Software, Inc. 2012-' . date('Y') . '. All Rights Reserved.',
         /**
          * Theme selection -- a bootswatch theme name
